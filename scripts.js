481d50247263cb914267ec66655557472f1ae0c4
Salvi Pascual <salvi.pascual@gmail.com>
2020-09-05 16:03:25 -0400

    new changes

--- scripts.js
+++ scripts.js
@@ -1,28 +1,40 @@
 // create the content for the slides
 var slides = [
-	{title:'Sube de nivel', body:'Usa la app y sube de nivel. Desbloquea beneficios y premios. Alcanza el nivel más alto y sé parte del club Diamante.'},
-	{title:'Gana crédito', body:'Usa la app toda la semana, invita amig@s, contesta en encuestas y concursos, canjea cupones, y gana crédito en la app.'},
-	{title:'Usa tu crédito', body:'Saca provecho a tu crédito adquiriendo tickets para las rifas, amuletos, recargas Cubacel, bonos especiales y más.'},
-	{title:'Crea tu perfil', body:'Agrega tu avatar, color, @username, provincia y descríbete para que otros conozcan tu estilo y sepan que te gusta.'},
-	{title:'Haz amig@s', body:'Opina públicamente en la Pizarra, conoce quienes piensa como tú, haz amig@s y habla en privado usando el Chat.'},
-	{title:'Busca parejas', body:'Encuentra tu media naranja en nuestra vibrante comunidad de parejas llamada Piropazo, tenemos miembros de toda Cuba.'},
-	{title:'Ponte al día', body:'Mantente informado sobre el acontecer nacional y mundial revisando los medios de prensa más leídos en Cuba actualmente.'},
-	{title:'Recibe ayuda', body:'¿Tienes dudas sobre cómo usar la app? Escríbenos al soporte y con gusto te responderemos en un máximo de 72 horas.'}
+	{title:'Hola, soy Apretín', body:'Soy la mascota de Apretaste, estoy acá para guiarte empezando a usar la app y ayudarte en lo que necesites.'},
+	{title:'Crea tu perfil', body:'Desde el menú accede a tu perfil. Agrega tu avatar, color, provincia y descríbete para que otros te conozcan.'},
+	{title:'Habla libremente', body:'Desde la barra superior, abre la Pizarra, nuestra red social donde pones lo que quieras y cuando quieras.'},
+	{title:'Haz amig@s', body:'Usando Pizarra conoce a otros que piensan como tú e invítales a ser tus amig@s. Revisa tus amig@s desde el menú.'},
+	{title:'Hazte popular', body:'Usando la app gana experiencia y sube en el ranking, llega al nivel Diamante, completa retos, dinámicas y concursos.'},
+	{title:'Usa tu crédito', body:'Canjea tu crédito por amuletos y adquiere poderes especiales, o por tickets para la rifa, o por recargas Cubacel.'},
+	{title:'Ahorra saldo', body:'En la pantalla inicial verás decenas de servicios de internet, optimizados para trabajar rápidamente y ahorrarte datos.'},
+	{title:'Recibe ayuda', body:'¿Tienes dudas sobre cómo funciona la app? Abre el servicio "Ayuda" y escríbeme; te responderé en menos de 72 horas.'}
 ];
 
 // current slide step
 var currentSlide = 0;
 
+// load the first slide on start
 $(document).ready(function(){
-	next();
+	update();
 });
 
-// load the next slide
-function next() {
-	// send users home on the last slide
-	if(currentSlide === 8) {
-		apretaste.send({'command':'SERVICIOS'});
-		return false;
+// update the slides and buttons 
+function update() {
+	// set the buttons
+	switch(currentSlide) {
+		case 0:
+			$('#btn-back').html('Saltar').off().click(close);
+			$('#btn-next').html('Comenzar').off().click(next);
+		break;
+
+		case 7:
+			$('#btn-back').html('Comenzar').off().click(close);
+			$('#btn-next').html('Ver tutorial').off().click(more);
+		break;
+
+		default:
+			$('#btn-back').html('Atrás').off().click(back);
+			$('#btn-next').html('Siguiente').off().click(next);
 	}
 
 	// set the text
@@ -31,14 +43,31 @@ function next() {
 
 	// set the image
 	$('.image').css('display', 'none');
-	$('.image-' + currentSlide)
-		.css('display', 'inline-block')
-		.attr('alt', slides[currentSlide].title);
+	$('.image-' + currentSlide).css('display', 'inline-block').attr('alt', slides[currentSlide].title);
+}
 
-	// set the button caption and click
-	if(currentSlide < 7) $('#btn-next').html('Siguiente');
-	else $('#btn-next').html('Comenzar');
-
-	// get to the next slide
+// move to the next slide
+function next() {
 	currentSlide++;
+	update();
+}
+
+// move to the previous slide
+function back() {
+	currentSlide--;
+	update();
+}
+
+// close the service
+function close() {
+	var command = isNewVersion ? 'INICIO' : 'SERVICIOS';
+	apretaste.send({command: command});
+}
+
+// read the tutorial
+function more() {
+	apretaste.send({
+		command: 'ESCUELA CURSO',
+		data: {query: tutorialId}
+	});
 }
